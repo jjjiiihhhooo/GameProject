361c8124faf3dd1bdaebd508b200655d539c0f79
YHNGHJ <520aliceinwonderland1951@gmail.com>
2022-10-07 00:52:41 +0900

    HSW
    + add playgoround_dummy image
    
    ?bed
        change FindOf... -> GetCompo...

--- Assets/Scripts/Event/LocalEvent/Bed.cs
+++ Assets/Scripts/Event/LocalEvent/Bed.cs
@@ -6,20 +6,21 @@ public class Bed : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private Transform bed;
-    [SerializeField] private Transform one;
+    [SerializeField] private Transform two;
     [SerializeField] private GameObject fadeout;
     private ChoiceEvent theChoiceEvent;
     private PlayerMove thePlayerMove;
 
     void Start()
     {
-        theChoiceEvent = FindObjectOfType<ChoiceEvent>();
+        theChoiceEvent = GetComponent<ChoiceEvent>();
         thePlayerMove = FindObjectOfType<PlayerMove>();
     }
     void Update()
     {
         if (theChoiceEvent.goEvent == true)
         {
+            Debug.Log("true");
             theChoiceEvent.goEvent = false;
             player.transform.position = bed.position;
             fadeout.SetActive(true);
@@ -32,7 +33,7 @@ public class Bed : MonoBehaviour
     {
         thePlayerMove.isMove = false;
         yield return new WaitForSeconds(1.0f);
-        player.transform.position = one.position;
+        player.transform.position = two.position;
         thePlayerMove.isMove = true;
     }
 }
